a42606d4b5e96edab4925b6cbddf3b0b3c2a91ce
husnuyasar <husnuyasar58@gmail.com>
2025-08-09 14:23:53 +0300

    📦🔌 Imported ConfigModule & TypeOrmModule to AppModule class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './db-config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [AppController],
   providers: [],
 })
