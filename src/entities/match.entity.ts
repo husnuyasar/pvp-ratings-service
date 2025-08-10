c0682100a64a0bf122394ae2c943957412489485
husnuyasar <husnuyasar58@gmail.com>
2025-08-10 15:00:53 +0300

    ✨🧩 Created `Match` entity class

---
+++ src/entities/match.entity.ts
@@ -0,0 +1,38 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Player } from './player.entity';
+
+@Entity({ name: 'matches' })
+export class Match {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @ManyToOne((type) => Player, (playerA) => playerA.id)
+  playerA: Player;
+
+  @ManyToOne((type) => Player, (playerB) => playerB.id)
+  playerB: Player;
+
+  @Column()
+  scoreA: number;
+
+  @Column()
+  scoreB: number;
+
+  @Column()
+  isDraw: boolean;
+
+  @Column('jsonb')
+  ratingsBefore: {
+    a: { mu: number; sigma: number };
+    b: { mu: number; sigma: number };
+  };
+
+  @Column('jsonb')
+  ratingsAfter: {
+    a: { mu: number; sigma: number };
+    b: { mu: number; sigma: number };
+  };
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
