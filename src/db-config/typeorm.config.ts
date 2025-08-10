88d02daf23aae3896f4c2fb025d85a76a96054cb
husnuyasar <husnuyasar58@gmail.com>
2025-08-10 15:03:11 +0300

    🔧🗂️ Added `migrations` property to DataSource/orm config

--- src/db-config/typeorm.config.ts
+++ src/db-config/typeorm.config.ts
@@ -4,16 +4,17 @@ import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from "@nestjs/typeorm
 export default class TypeOrmConfig {
     static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
         return {
-            type: 'postgres',
-            host: configService.get<string>('DB_HOST'),
-            port: configService.get<number>('DB_PORT'),
-            username: configService.get<string>('DB_USERNAME'),
-            password: configService.get<string>('DB_PASSWORD'),
-            database: configService.get<string>('DB_NAME'),
-            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-            synchronize: false,
-            logging: true,
-        }
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/../migrations/*.ts'],
+          synchronize: false,
+          logging: true,
+        };
     }
 }
 
