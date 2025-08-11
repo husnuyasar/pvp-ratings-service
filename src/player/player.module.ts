d7638ec980ca24cc327a73c3089ba0f171a64717
husnuyasar <husnuyasar58@gmail.com>
2025-08-11 22:46:08 +0300

    ✨🧩 Created PlayerModule class

---
+++ src/player/player.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PlayerService } from './player.service';
+import { PlayerController } from './player.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Player } from 'src/entities/player.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Player])
+  ],
+  providers: [PlayerService],
+  controllers: [PlayerController]
+})
+export class PlayerModule {}
