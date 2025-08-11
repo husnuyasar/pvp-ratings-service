2b8facde8b22cb3d425f8cb2cfd36d385b7791b5
husnuyasar <husnuyasar58@gmail.com>
2025-08-11 22:47:54 +0300

    🧩📥 Imported PlayerModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './db-config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
+import { PlayerModule } from './player/player.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    PlayerModule,
   ],
   controllers: [AppController],
   providers: [],
