026d552399a931a6ebe19ac2d89e4dcae2b1cc27
husnuyasar <husnuyasar58@gmail.com>
2025-08-11 21:12:32 +0300

    ✨📚 Added OpenAPI/Swagger configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('PvP Ratings API')
+    .setVersion('1.0')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, doc, {
+    swaggerOptions: { persistAuthorization: true },
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
