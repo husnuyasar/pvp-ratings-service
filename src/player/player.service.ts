1a63644923f148855a542cc03655651cada05e7b
husnuyasar <husnuyasar58@gmail.com>
2025-08-12 21:45:52 +0300

    🔍👤 Added "get" method to fetch player by ID

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -10,22 +10,32 @@ export class PlayerService {
     private readonly playerRepository: Repository<Player>,
   ) {}
 
+  async get(id: string): Promise<Player> {
+    const player = await this.playerRepository.findOne({ where: { id } });
+    if (!player)
+      throw new HttpException(
+        'Player not found',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    return player;
+  }
+
   async top10(): Promise<any> {
     try {
-        return await this.playerRepository
-          .createQueryBuilder('p')
-          .select('p.id', 'id')
-          .addSelect('p.username', 'name')
-          .addSelect('p.rating', 'rating')
-          .orderBy('p.rating', 'DESC')
-          .limit(10)
-          .getRawMany<{
-            id: string;
-            name: string;
-            rating: number;
-          }>();
+      return await this.playerRepository
+        .createQueryBuilder('p')
+        .select('p.id', 'id')
+        .addSelect('p.username', 'name')
+        .addSelect('p.rating', 'rating')
+        .orderBy('p.rating', 'DESC')
+        .limit(10)
+        .getRawMany<{
+          id: string;
+          name: string;
+          rating: number;
+        }>();
     } catch (error) {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 }
