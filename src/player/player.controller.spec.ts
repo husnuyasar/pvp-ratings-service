e971d77171d3b0685eddb6c1728c03510d724dad
husnuyasar <husnuyasar58@gmail.com>
2025-08-12 22:00:39 +0300

    🧪🆔 Added unit test for GET /players/:id (UUID validation) in player.controller.spec.ts

--- src/player/player.controller.spec.ts
+++ src/player/player.controller.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayerController } from './player.controller';
+import { HttpStatus, ParseUUIDPipe } from '@nestjs/common';
+import { PlayerService } from './player.service';
 
 describe('PlayerController', () => {
   let controller: PlayerController;
+  let service: jest.Mocked<PlayerService>;
 
   beforeEach(async () => {
+    const serviceMock: Partial<jest.Mocked<PlayerService>> = {
+      get: jest.fn(),
+    }
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PlayerController],
+      providers: [{
+        provide: PlayerService, useValue: serviceMock
+      }]
     }).compile();
 
     controller = module.get<PlayerController>(PlayerController);
+    service = module.get<PlayerService>(PlayerService) as jest.Mocked<PlayerService>;
+    jest.clearAllMocks();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('ParseUUIDPipe validations', async () => {
+    const pipe = new ParseUUIDPipe({ version: '4' });
+
+    // correct one
+    await expect(
+      pipe.transform('d9ce350e-029a-44c0-a878-a3c64db6d359', {
+        type: 'param',
+        metatype: String,
+        data: 'id',
+      }),
+    ).resolves.toBe('d9ce350e-029a-44c0-a878-a3c64db6d359');
+
+    // wrong id
+    await expect(
+      pipe.transform('fake-uuid', {
+        type: 'param',
+        metatype: String,
+        data: 'id',
+      }),
+    ).rejects.toMatchObject({ status: HttpStatus.BAD_REQUEST });
   });
 });
