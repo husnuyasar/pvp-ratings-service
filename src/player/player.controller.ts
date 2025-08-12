88145282382550503d2891044fb916714e934990
husnuyasar <husnuyasar58@gmail.com>
2025-08-12 21:44:34 +0300

    🔎👤 Added "get" GET endpoint to fetch player by ID

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { PlayerService } from './player.service';
 
 @Controller('player')
 @ApiTags('Player')
 export class PlayerController {
-    constructor(
-        private readonly playerService: PlayerService
-    ) {}
+  constructor(private readonly playerService: PlayerService) {}
 
-    @Get('top10')
-    async top10() {
-        return await this.playerService.top10();
-    }
+  @Get(':id')
+  @ApiParam({ name: 'id', description: 'Player ID', format: 'uuid' })
+  get(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.playerService.get(id);
+  }
+
+  @Get('top10')
+  async top10() {
+    return await this.playerService.top10();
+  }
 }
