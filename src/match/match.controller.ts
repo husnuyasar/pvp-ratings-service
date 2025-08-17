6654205bc5cd2ca84670ba7843260fe7d0049083
husnuyasar <husnuyasar58@gmail.com>
2025-08-17 17:50:30 +0300

    ✨🌐 Added "getAll" (GET /matches) endpoint in MatchController

--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { CreateMatchDto } from './dto/create-match.dto';
 
 @Controller('match')
 export class MatchController {
-    constructor(private readonly matchService: MatchService)
-    {}
+  constructor(private readonly matchService: MatchService) {}
 
-    @Post()
-    async create(@Body() dto: CreateMatchDto) {
-        return await this.matchService.create(dto);
-    }
+  @Get()
+  async getAll() {
+    return await this.matchService.getAll();
+  }
+  
+  @Post()
+  async create(@Body() dto: CreateMatchDto) {
+    return await this.matchService.create(dto);
+  }
 }
