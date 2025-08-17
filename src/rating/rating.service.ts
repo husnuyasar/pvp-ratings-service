f6f187e0a6b147bee07ec78b8ea2af6adc61ef66
husnuyasar <husnuyasar58@gmail.com>
2025-08-17 17:30:38 +0300

    ✨🚀 Added computePlayers method to RatingService

--- src/rating/rating.service.ts
+++ src/rating/rating.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { ordinal, rating, Rating } from 'openskill';
+import { ordinal, rate, rating, Rating } from 'openskill';
+import { ComputeDto } from './dto/compute.dto';
+import { ComputeResponseDto } from './dto/compute.response.dto';
 
 @Injectable()
 export class RatingService {
-    default(): Rating {
-        return rating();
-    }
+  default(): Rating {
+    return rating();
+  }
 
-    calRating(rating: Rating): number {
-        return ordinal(rating);
-    }
+  calRating(rating: Rating): number {
+    return ordinal(rating);
+  }
+
+  computePlayers(dto: ComputeDto): ComputeResponseDto {
+    const aBefore = rating({ mu: dto.aMu, sigma: dto.aSigma });
+    const bBefore = rating({ mu: dto.bMu, sigma: dto.bSigma });
+
+    const ranks =
+      dto.outcome === 'D' ? [0, 0] : dto.outcome === 'A' ? [0, 1] : [1, 0];
+    const [[aAfter], [bAfter]] = rate([[aBefore], [bBefore]], { rank: ranks });
+
+    return {
+      aBefore,
+      bBefore,
+      aAfter,
+      bAfter,
+      aScore: ordinal(aAfter),
+      bScore: ordinal(bAfter),
+    } as ComputeResponseDto;
+  }
 }
