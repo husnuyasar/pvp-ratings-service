03ccf816a94aae40152a9c09e07c5677e014d9a9
husnuyasar <husnuyasar58@gmail.com>
2025-08-17 17:22:24 +0300

    ✨⚙️ Imported MatchModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './db-config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { PlayerModule } from './player/player.module';
+import { MatchModule } from './match/match.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     PlayerModule,
+    MatchModule,
   ],
   controllers: [AppController],
   providers: [],
